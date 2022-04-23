5cbdb88b6a2a75e56f7679460d7578e4fd84a106
Dmitriy Tsyplov <d.tsyplov@mail.ru>
2022-04-23 09:46:25 +0200

    dog card

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,34 +1,52 @@
-import  React from 'react';
+import  React,{useEffect, useState} from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import axios from 'axios';
+import { FlatList } from 'react-native-gesture-handler';
 export function HomeScreen({navigation}) {
+  const [url, setUrl] = useState();
+  const [name, setName] = useState()
+  const [breeds, setBreeds] = useState()
+
   const loadData = async ()=> {
-    const image = await axios.get('https://api.thecatapi.com/v1/images/search');
-    const data = image.data;
-    const i = data[0];
-   
-    console.log(data)
+    const breeds = await axios.get('https://api.thedogapi.com/v1/breeds');
+    const data = breeds.data;
+    const dog = data[4];
+    setUrl(dog .image.url);
+    setName(dog .name)
+    setBreeds(data)
+    console.log('PPPPPPP', dog )
+    
   
   }
-  loadData();
-  const renderItem = ()=> {
+
+  useEffect(()=> loadData(), [])
+  
+  const renderItem = ({item})=> {
     return (
       <View style={styles.block}>
-    <Image source={{uri:"https://picsum.photos/200", width:100, height:100 }}/>
+    <Image source={{uri: item.image.url, width:100, height:100 }}/>
     <View style={styles.dogTextWrapper}>
-      <Text style={styles.dogTitle}>Breed</Text>
-      <Text style={styles.dogText}>Dog description</Text>
+      <Text style={styles.dogTitle}>{item.name}</Text>
+      <Text style={styles.dogText}>{item.temperament}</Text>
     </View>
       </View>
     )
   }
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', marginTop: 60, }}>
       <Text>HomeScreen</Text>
     <Button
       title="Go to Favorites"
       onPress={() => navigation.navigate('Favorites')}
     />
+    <FlatList
+    data={breeds}
+    renderItem={renderItem}
+    />
+      {/* <Button
+      title="Load data"
+      onPress={() => loadData()}
+    /> */}
     
   </View>
   
@@ -41,24 +59,27 @@ const styles = StyleSheet.create({
   },
 
   dogTitle: {
+    width: 190,
     fontFamily: "Cochin",
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: "bold",
     textTransform: 'uppercase',
   },
 
   dogText: {
     fontFamily: "Cochin",
-    fontSize: 14,
+    fontSize: 16,
+    width: 190,
   },
 
   block: {
    padding: 10,
    flexDirection: 'row',
-   width: '90%',
+   width: '100%',
    borderColor: '#000',
    borderWidth: 1,
    borderRadius: 10,
+   marginBottom: 10,
   },
 
   container: {
